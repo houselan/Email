883f57acae755d1a830414082ae7ec1eb2959023
20211TIIMI0249 <paulocezarrf@hotmail.com>
2023-03-17 10:03:06 -0300

    Initial commit

---
+++ app/src/main/java/furtado/rocha/cezar/paulo/email/MainActivity.java
@@ -0,0 +1,14 @@
+package furtado.rocha.cezar.paulo.email;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
